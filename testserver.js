03d891a62d2d72b8228852271215583d5f866278
Kuriosa84 <Kuriosa84>
2018-11-22 19:17:59 +0100

    fixed the nodejs code so it should work

--- testserver.js
+++ testserver.js
@@ -4,7 +4,7 @@ var port = 3000;
 
 var app = express();
 
-var server = require('http').Server(app);
+//var server = require('http').Server(app);
 var db;
 
 mongo.connect('mongodb://kuriosa:kuriosa123@ds233763.mlab.com:33763/kuriosatest', function(err, database) {
@@ -12,23 +12,34 @@ mongo.connect('mongodb://kuriosa:kuriosa123@ds233763.mlab.com:33763/kuriosatest'
         console.log(err);
     }
     db = database;
+    console.log("Connected to mongo?");
 });
 
-server.listen(port, function() {
-    // Listening on port
-});
+app.get('/test', function(req, res) {
+    console.log("Trying to find barcode");
+    //var code = req.params.barcode;
+    var code = req.query.barcode;
 
-app.get('/test/:barcode', function(req, res) {
-    var code = req.params.barcode;
-
-    db.collection('test').findOne({barcode: code}, function(err, name) {
+    db.collection('test').findOne({barcode: code}, function(err, obj) {
         if(err) {
             res.status(500).send(err);
-        } else if(name == null) {
+            console.log("Error");
+        } else if(obj == null) {
             res.status(401).send({});
+            console.log("Name is null");
         } else {
-            var responseObject = { name: name };
-            res.status(200).send(responseObject);
+            res.status(200).send(obj.name);
+            console.log("Yaaay!");
         }
     });
+});
+
+app.get('/', function(req, res) {
+    res.send('Test');
+    console.log("Testar");
+});
+
+app.listen(port, function() {
+    // Listening on port
+    console.log("Listening on port 3000");
 });
